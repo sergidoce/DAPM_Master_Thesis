c6f1602a0a427182c7759cb27a8f87496b9d347a
s223123 <s223123@dtu.dk>
2024-04-17 18:22:29 +0200

    OperatorMS added and is a working demo

---
+++ DAPM/DAPM.OperatorMS.Api/Controllers/OperatorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAPM.OperatorMS.Api.Services.Interfaces;
+
+namespace DAPM.OperatorMS.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OperatorController : ControllerBase
+    {
+        private readonly ILogger<OperatorController> _logger;
+        private IOperatorService _operatorService;
+
+        public OperatorController(ILogger<OperatorController> logger, IOperatorService operatorService) { 
+            _logger = logger;
+            _operatorService = operatorService;
+        }
+
+        [HttpGet(Name = "operator")]
+        public async Task<IActionResult> Get([FromQuery] string operatorName, [FromQuery] string parameter) {
+            byte[] pngImageBytes = await _operatorService.ExecuteMiner(operatorName, parameter);
+
+            return File(pngImageBytes, "image/png");
+        }
+    }
+}
