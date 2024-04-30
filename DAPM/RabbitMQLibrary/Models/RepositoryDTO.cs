8133e4dfe808236c3d78b2eb61f212785221e896
Sergi <sergidoce@gmail.com>
2024-04-30 10:02:02 +0200

    implementation of GetOrganizationById and GetRepositoriesOfOrg

--- DAPM/RabbitMQLibrary/Models/RepositoryDTO.cs
+++ DAPM/RabbitMQLibrary/Models/RepositoryDTO.cs
@@ -8,7 +8,7 @@ namespace RabbitMQLibrary.Models
 {
     public class RepositoryDTO
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
