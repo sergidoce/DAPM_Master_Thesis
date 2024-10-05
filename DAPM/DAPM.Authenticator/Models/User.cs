2de735c525d68eacd5eb89fb9f3bad390a72bdae
SuhailDTU <s205135@student.dtu.dk>
2024-10-05 14:25:59 +0200

    Accidentially didnt add property accessors to fullname property causing the EF to not make a row for it.

--- DAPM/DAPM.Authenticator/Models/User.cs
+++ DAPM/DAPM.Authenticator/Models/User.cs
@@ -7,6 +7,6 @@ namespace DAPM.Authenticator.Models
     public class User : IdentityUser<int>
     {
         public ICollection<UserRole> UserRoles { get; set; }
-        public string FullName;
+        public string FullName { get; set; }
     }
 }
