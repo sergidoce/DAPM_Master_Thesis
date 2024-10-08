1ca46a9e4be346b3c6a2d4df95f27ce30c890df5
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-10-08 10:03:02 +0200

    added an empty api endpoint for authentication

---
+++ DAPM/DAPM.ClientApi/Services/Interfaces/IAuthenticatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace DAPM.ClientApi.Services.Interfaces
+{
+    public interface IAuthenticatorService
+    {
+        public void SignUp();
+        public void LogIn();
+        public void AddUser();
+        public void RemoveUser();
+    }
+}
